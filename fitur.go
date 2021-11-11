d01bbe2e9009935ecebe5dc59ec1b7a47b130ad3
nurdin-sapariansyah <nurdin27muhammad@gmail.com>
2021-11-11 12:25:49 +0800

    menambahkan fitur a

--- fitur.go
+++ fitur.go
@@ -10,6 +10,10 @@ func main(){
 	fmt.Print(i)
 	fmt.Println("---------->>Identitas saya<<---------------")
 	//fiturA
+	name := "M. nurdin sapariansyah"
+	age := 17
+
+	fmt.Println(name, age)
 	fmt.Println("---------->>Background saya<<---------------")
 	//fiturB
 	
